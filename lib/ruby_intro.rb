95accf0560fa384681a6b4d429be1cb85e5c1477
EC2 Default User <ec2-user@ip-172-31-20-169.us-east-2.compute.internal>
2023-02-09 15:59:46 +0000

    final

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,33 +4,125 @@
 
 def sum(arr)
   # YOUR CODE HERE
+  sumAll = 0
+  for i in arr do 
+    sumAll = sumAll + i
+  end
+  sumAll
 end
 
 def max_2_sum(arr)
   # YOUR CODE HERE
+  sorted = arr.sort {|x, y| y <=> x}
+  result = 0
+  len = arr.length()
+  if len > 1
+    result = sorted[0] + sorted[1]
+  else
+    if len == 1
+      result = arr[0]
+    else
+      result = 0
+    end
+  end
+  result  
 end
+
 
 def sum_to_n?(arr, number)
   # YOUR CODE HERE
+  result = false
+  if arr.length() > 1
+    test = arr.combination(2).find{|x,y| x+y==number}
+    if test != nil
+      result = true
+    end  
+  else
+    if arr.length() == 1
+      result = false
+    else
+      result = false
+    end
+  end
+  result
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  result = "Hello, #{name}"
+  result
 end
+
+def letter?(lookAhead)
+  lookAhead.match?(/[[:alpha:]]/)
+end
+
 
 def starts_with_consonant?(string)
   # YOUR CODE HERE
+  vowel = 'AEIOUaeiou'
+  result = true
+  if vowel.include? string[0]
+    result = false
+  end  
+  if string.empty?
+    result = false
+  end
+  result
 end
 
 def binary_multiple_of_4?(string)
   # YOUR CODE HERE
+  check = string !~ /[^01]/
+  result = false
+  if check and string != ""
+    integer = string.to_i(2)
+    if integer % 4 == 0
+      result = true
+    end
+  end 
+  result
 end
 
+string = '#C'
+vowel = 'AEIOUaeiou'
+check = vowel.include? string[0]
+print(check)
 # Part 3
 
 # Object representing a book
 class BookInStock
   # YOUR CODE HERE
+  def initialize(isbn_,price_)\
+    if(isbn_ == "" or price_ <= 0)
+      raise ArgumentError
+    end
+    
+    @isbn = isbn_
+    @price = price_
+  end
+  
+  def isbn=(isb)
+    @isbn = isb
+  end
+  
+  def price=(pric)
+    @price = pric
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def price_as_string
+    result = "$%0.2f" % [price]
+    result
+  end
+  
 end
